87d5c293ee9ee2df5661bd0d237e5cb6f83c5b0e
Amina <minassouane@gmail.com>
2026-02-23 16:35:54 +0100

    Connect to database

--- db/populatedb.js
+++ db/populatedb.js
@@ -14,3 +14,24 @@ const createCategoriesData = `INSERT INTO categories (category) VALUES
 
 const createItemsData = `INSERT INTO jewels (name, category_id, quantity, price) VALUES
 ('Ruby', 1, 4, 255), ('Flowery pendant', 2, 11, 23), ('Friendship bracelet', 3, 55, 7), ('Diamond ring', 4, 2, 1432)`;
+
+async function main() {
+  const client = new Client();
+  try {
+    await client.connect();
+    console.log("Connected to database.");
+    await client.query(createCategoriesTable);
+    await client.query(createItemsTable);
+    console.log("Tables created.");
+    await client.query(createCategoriesData);
+    await client.query(createItemsData);
+    console.log("Data created.");
+  } catch (error) {
+    console.error("Error occured:", error);
+  } finally {
+    await client.end();
+    console.log("Done.");
+  }
+}
+
+main();

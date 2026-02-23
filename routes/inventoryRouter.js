8d99b7edd0de7eb338a9764f6e0495ff4e29df22
Amina <minassouane@gmail.com>
2026-02-23 11:47:28 +0100

    Add specific routes

--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -12,9 +12,17 @@ inventoryRouter.get("/categories", (req, res) => {
   res.send("Categories");
 });
 
+inventoryRouter.get("/categories/:id", (req, res) => {
+  res.send("Category");
+});
+
 // Items
 inventoryRouter.get("/items", (req, res) => {
   res.send("Items");
+});
+
+inventoryRouter.get("/items/:id", (req, res) => {
+  res.send("Item");
 });
 
 module.exports = inventoryRouter;

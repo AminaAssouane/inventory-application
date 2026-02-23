05e459dc39f0213b01aa5652b0008dd1a887dedf
Amina <minassouane@gmail.com>
2026-02-23 09:25:15 +0100

    Add all routes

--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -1,9 +1,20 @@
+// routes/inventoryRouter.js
 const { Router } = require("express");
 const inventoryRouter = Router();
 
 // Homepage
 inventoryRouter.get("/", (req, res) => {
   res.send("Homepage");
+});
+
+// Categories
+inventoryRouter.get("/categories", (req, res) => {
+  res.send("Categories");
+});
+
+// Items
+inventoryRouter.get("/items", (req, res) => {
+  res.send("Items");
 });
 
 module.exports = inventoryRouter;

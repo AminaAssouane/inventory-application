677e95302de7a710d5964efeab6b468ebc435055
Amina <minassouane@gmail.com>
2026-02-23 17:07:50 +0100

    Add connection string

--- db/populatedb.js
+++ db/populatedb.js
@@ -10,13 +10,15 @@ name VARCHAR (255), category_id INTEGER, quantity INTEGER, price INTEGER )`;
 
 // Populating tables
 const createCategoriesData = `INSERT INTO categories (category) VALUES 
-('Gemstone'), ('Necklace), ('Bracelet'), ('Ring')`;
+('Gemstone'), ('Necklace'), ('Bracelet'), ('Ring')`;
 
 const createItemsData = `INSERT INTO jewels (name, category_id, quantity, price) VALUES
 ('Ruby', 1, 4, 255), ('Flowery pendant', 2, 11, 23), ('Friendship bracelet', 3, 55, 7), ('Diamond ring', 4, 2, 1432)`;
 
 async function main() {
-  const client = new Client();
+  const client = new Client({
+    connectionString: "postgresql://aminaassouane:null@localhost:5432/jewelry",
+  });
   try {
     await client.connect();
     console.log("Connected to database.");

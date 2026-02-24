ce7513d68bd2776afb33e831faf3441b8ea27fff
Amina <minassouane@gmail.com>
2026-02-24 02:05:42 +0100

    Implement first query

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,4 @@
-//controllers/inventoryController.js
+const db = require("../db/queries");
 
 function homepage(req, res) {
   res.render("home");
@@ -8,13 +8,24 @@ function getCategories(req, res) {
   res.render("categories");
 }
 
-function getCategory(req, res) {
+async function getCategory(req, res) {
   const id = req.params.id;
-  res.render("category", { id: id });
+  try {
+    const category = await db.getCategory(id);
+    if (!category) {
+      return res.status(404).send("Category not found");
+    }
+    console.log("Category: ", category.category);
+    res.render("category", { category });
+  } catch (error) {
+    console.error("Error fetching category:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
-function getItems(req, res) {
-  res.render("items");
+async function getItems(req, res) {
+  const items = await db.getAllNames();
+  res.send("items : " + items.map((item) => item.name).join(" & "));
 }
 
 function getItemById(req, res) {
---
+++ db/queries.js
@@ -0,0 +1,16 @@
+const pool = require("./pool");
+
+async function getAllNames() {
+  const { rows } = await pool.query("SELECT name FROM jewels");
+  return rows;
+}
+
+async function getCategory(id) {
+  const { rows } = await pool.query(
+    "SELECT category FROM categories WHERE id = $1",
+    [id],
+  );
+  return rows[0] || null;
+}
+
+module.exports = { getAllNames, getCategory };
